760486b2b33fea29a623477e34943542f3c8f8f7
ADAGE\ana.blanco <ana.blanco@adagetech.com>
2024-10-29 22:13:48 -0500

    Adding more components

--- app-animal/src/App.jsx
+++ app-animal/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import AnimalList from './components/AnimalList';
+import './style/App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Adopt a Pet</h1>
-      <AnimalList />
+    <div className="App">
+      <header className="header">
+        <h1>¡Adopta a tu mejor amigo!</h1>
+        <p>Encuentra tu aliado perfecto para cada aventura.</p>
+      </header>
+      <main>
+        <AnimalList />
+      </main>
+      
     </div>
   );
 }
 
 export default App;
-
---
+++ app-animal/src/redux/animalComponente.jsx
@@ -0,0 +1,32 @@
+// redux/animalComponente.jsx
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchCats } from '../services/catService';
+import { fetchDogs } from '../services/dogService';
+
+export const loadAnimals = createAsyncThunk('animals/loadAnimals', async () => {
+  const cats = await fetchCats();
+  const dogs = await fetchDogs();
+  return { cats, dogs };
+});
+
+const animalsSlice = createSlice({
+  name: 'animal',
+  initialState: { cats: [], dogs: [], status: 'idle' },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadAnimals.pending, (state) => {
+        state.status = 'cargando';
+      })
+      .addCase(loadAnimals.fulfilled, (state, action) => {
+        state.cats = action.payload.cats;
+        state.dogs = action.payload.dogs;
+        state.status = 'exitoso';
+      })
+      .addCase(loadAnimals.rejected, (state) => {
+        state.status = 'fallo';
+      });
+  },
+});
+
+export default animalsSlice.reducer;
